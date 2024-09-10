050815b81b5e514c3dc07a9ece17493579d5073a
Psychopath21312 <rodionr07@gmail.com>
2024-09-10 23:48:13 +0400

    кнопачка,ыыыыы

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import random as rand
 class Main(Frame):
     def __init__(self,root):
         super().__init__(root)
-but1 = Button(text='Сгенерировать число')
-but1.place(x=10,y=10)
+        self.init_main()
+
+    def init_main(self):
+        global btn1
+        btn1 = Button(text='Сгенерировать число', command=self.click_btn)
+        btn1.pack()
+
+    def click_btn(self):
+        global btn1
+        btn1['text'] = str(rand.randint(0, 100))
+        
 
 if __name__ == '__main__':
     root = Tk()
     app = Main(root)
     root.title('Генератор случайных чисел')
     root.geometry('800x600')
-    root.mainloop()
+    app.mainloop()
