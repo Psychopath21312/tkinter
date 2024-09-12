9b44e0c4995856e1cf10b05f95e1297e15ea0b6c
Psychopath21312 <rodionr07@gmail.com>
2024-09-12 22:52:46 +0400

    Хуйня.Переделывай.#2

--- main.py
+++ main.py
@@ -6,148 +6,158 @@ class Main(Frame):
         self.init_main()
 
     def init_main(self):
-        self.hand = ''
+        self.Dx = ''
+        self.math = ''
 
-        self.check = Label(text='', width=60, height='4', font=('Arial', 12, 'bold'), bg='#d7d7d7')
+        self.check = Label(text='', width=60, font=('Arial', 12, 'bold'), bg='#d7d7d7')
 
-        self.btn_one = Button(text='1', command=self.click_one)
-        self.btn_two = Button(text='2', command=self.click_two)
-        self.btn_three = Button(text='3', command=self.click_three)
-        self.btn_four = Button(text='4', command=self.click_four)
-        self.btn_five = Button(text='5', command=self.click_five)
-        self.btn_six = Button(text='6', command=self.click_six)
-        self.btn_seven = Button(text='7', command=self.click_seven)
-        self.btn_eight = Button(text='8', command=self.click_eight)
-        self.btn_nine = Button(text='9', command=self.click_nine)
-        self.btn_nil = Button(text='0', command=self.click_nil)
+        self.btn_one = Button(text='1', width=5, height=5, command=self.click_one)
+        self.btn_two = Button(text='2', width=5, height=5, command=self.click_two)
+        self.btn_three = Button(text='3', width=5, height=5, command=self.click_three)
+        self.btn_four = Button(text='4', width=5, height=5, command=self.click_four)
+        self.btn_five = Button(text='5', width=5, height=5, command=self.click_five)
+        self.btn_six = Button(text='6', width=5, height=5, command=self.click_six)
+        self.btn_seven = Button(text='7', width=5, height=5, command=self.click_seven)
+        self.btn_eight = Button(text='8', width=5, height=5, command=self.click_eight)
+        self.btn_nine = Button(text='9', width=5, height=5, command=self.click_nine)
+        self.btn_nil = Button(text='0', width=5, height=5, command=self.click_nil)
 
-        self.btn_sum = Button(text='+', command=self.click_sum)
-        self.btn_dif = Button(text='-', command=self.click_dif)
+        self.btn_sum = Button(text='+', width=7, height=5, command=self.click_sum)
+        self.btn_dif = Button(text='-', width=7, height=5, command=self.click_dif)
 
-        self.result = Button(text='=', command=self.result_bt)
+        self.result = Button(text='=', width=5, height=5, command=self.result_bt)
 
-        self.del_bt = Button(text='<---', command=self.del_check)
+        self.del_bt = Button(text='<---', width=30, height=5, command=self.del_check)
 
-        self.check.pack()
-        self.btn_one.pack()
-        self.btn_two.pack()
-        self.btn_three.pack()
-        self.btn_four.pack()
-        self.btn_five.pack()
-        self.btn_six.pack()
-        self.btn_seven.pack()
-        self.btn_eight.pack()
-        self.btn_nine.pack()
-        self.btn_nil.pack()
-        self.btn_sum.pack()
-        self.btn_dif.pack()
-        self.result.pack()
-        self.del_bt.pack()
+        self.check.grid(column=2, row=1)
+        self.btn_one.grid(column=1, row=2)
+        self.btn_two.grid(column=2, row=2)
+        self.btn_three.grid(column=3, row=2)
+        self.btn_four.grid(column=1, row=3)
+        self.btn_five.grid(column=2, row=3)
+        self.btn_six.grid(column=3, row=3)
+        self.btn_seven.grid(column=1, row=4)
+        self.btn_eight.grid(column=2, row=4)
+        self.btn_nine.grid(column=3, row=4)
+        self.btn_nil.grid(column=1, row=5)
+        self.btn_sum.grid(column=2, row=5)
+        self.btn_dif.grid(column=3, row=5)
+        self.result.grid(column=1, row=6)
+        self.del_bt.grid(column=2, row=6)
 
     def click_one(self):
-        self.hand += '1'
-        one = self.hand
-        for i in range(len(one)+1, 1, -1):
-            if i % 3 == 0:
-                one = f'{one[:i]} {one[i:]}'
-        self.check['text'] = one
+        self.math += '1'
+        self.Dx += '1'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_two(self):
-        self.hand += '2'
-        two = self.hand
-        for i in range(len(two)+1, 1, -1):
-            if i % 3 == 0:
-                two = f'{two[:i]} {two[i:]}'
-        self.check['text'] = two
+        self.math += '2'
+        self.Dx += '2'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_three(self):
-        self.hand += '3'
-        three = self.hand
-        for i in range(len(three)+1, 1, -1):
-            if i % 3 == 0:
-                three = f'{three[:i]} {three[i:]}'
-        self.check['text'] = three
+        self.math += '3'
+        self.Dx += '3'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_four(self):
-        self.hand += '4'
-        four = self.hand
-        for i in range(len(four)+1, 1, -1):
-            if i % 3 == 0:
-                four = f'{four[:i]} {four[i:]}'
-        self.check['text'] = four
+        self.math += '4'
+        self.Dx += '4'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_five(self):
-        self.hand += '5'
-        five = self.hand
-        for i in range(len(five)+1, 1, -1):
-            if i % 3 == 0:
-                five = f'{five[:i]} {five[i:]}'
-        self.check['text'] = five
+        self.math += '5'
+        self.Dx += '5'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_six(self):
-        self.hand += '6'
-        six = self.hand
-        for i in range(len(six)+1, 1, -1):
-            if i % 3 == 0:
-                six = f'{six[:i]} {six[i:]}'
-        self.check['text'] = six
+        self.math += '6'
+        self.Dx += '6'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_seven(self):
-        self.hand += '7'
-        seven = self.hand
-        for i in range(len(seven)+1, 1, -1):
-            if i % 3 == 0:
-                seven = f'{seven[:i]} {seven[i:]}'
-        self.check['text'] = seven
+        self.math += '7'
+        self.Dx += '7'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_eight(self):
-        self.hand += '8'
-        eight = self.hand
-        for i in range(len(eight)+1, 1, -1):
-            if i % 3 == 0:
-                eight = f'{eight[:i]} {eight[i:]}'
-        self.check['text'] = eight
+        self.math += '8'
+        self.Dx += '8'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_nine(self):
-        self.hand += '9'
-        nine = self.hand
-        for i in range(len(nine)+1, 1, -1):
-            if i % 3 == 0:
-                nine = f'{nine[:i]} {nine[i:]}'
-        self.check['text'] = nine
+        self.math += '9'
+        self.Dx += '9'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_nil(self):
-        self.hand += '0'
-        nil = self.hand
-        for i in range(len(nil)+1, 1, -1):
-            if i % 3 == 0:
-                nil = f'{nil[:i]} {nil[i:]}'
-        self.check['text'] = nil
+        self.math += '0'
+        self.Dx += '0'
+        display = self.Dx
+        for i in range(len(self.Dx), 0, -3):
+            display = f'{display[:i]} {display[i:]}'
+        self.check['text'] = display
 
     def click_sum(self):
-        if self.hand != '':
-            self.hand += ' + '
+        self.Dx = ''
+        if self.math != '':
+            self.math += ' + '
             self.check['text'] += ' + '
 
     def click_dif(self):
-        if self.hand != '':
-            self.hand += ' - '
+        self.Dx = ''
+        if self.math != '':
+            self.math += ' - '
             self.check['text'] += ' - '
 
     def result_bt(self):
-        self.hand = str(eval(self.hand))
-        resul = self.hand
-        for i in range(len(resul)+1, 1, -1):
-            if i % 3 == 0:
+        self.math = str(eval(self.math))
+        resul = self.math
+        if int(resul) >= 0:
+            for i in range(len(resul), 0, -3):
                 resul = f'{resul[:i]} {resul[i:]}'
-                self.check['text'] = resul
+        elif int(resul) < 0:
+            magic = str(int(resul) * -1)
+            for i in range(len(magic), 0, -3):
+                resul = f'{resul[:i+1]} {resul[i+1:]}'
+        self.check['text'] = resul
+        self.Dx = resul
 
     def del_check(self):
+        self.Dx = ''
         self.check['text'] = ''
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Калькулятор')
-    root.geometry('800x600')
+    root.geometry('800x700')
     app = Main(root)
+    root.resizable(False, False)
     app.mainloop()
